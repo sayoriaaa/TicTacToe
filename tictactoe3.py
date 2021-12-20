eb7830bec67d3657101f256a7165c4242d61fcb2
Li YiQing <73006417+sayoriaaa@users.noreply.github.com>
2021-12-20 11:14:04 +0800

    Add files via upload

--- tictactoe3.py
+++ tictactoe3.py
@@ -4,22 +4,45 @@ Created on Tue Dec 14 19:22:33 2021
 
 @author: sayori
 
-去掉了之前漫天飞的全局变量，改成类的数据
-感觉有点下意识地写工厂模式了(面向对象真香)
-实现了之前想的一个小idea:套用卷积的方法来实现模式的匹配
-当然速度又慢了一点，但是实现了N字棋与棋盘sizeM的MN自由(m=5就不行了,m等于4勉强能玩)
-但m=4的情况是有问题的，因为
-井字棋计算机后手是一定不会输的
-所以没有考虑可能会输的选择
-但是m=4的情况计算机基本上算出来会输，这时候要选择加上即使是失败也要最努力的方向而不是直接随机躺平
-不然就看不出智能了(怎么感觉努力后的躺平也是一种智能行为 哭)
-暂时的思路是如果全部返回为-1的话就把min改为随机
+关于模型的保存，本来用json的，结果发现只能使用pickle，json不支持类的存储
+pickle只能以二进制读写，但是可以自定义文件名，好耶
+考虑到其他类要调用当前游戏进程的数据
+因为game类只创建一次，将参数提到类变量
+
+
 
 """
 import tkinter
 import numpy as np
 import time
 import random
+import pickle
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torch.utils.data as torch_data
+
+
+
+def get_matrix_index(a,value):
+    ''' 
+    非主要内容
+    不想自己写的，但是搜了一圈numpy的where真是太不好用了，还不如自己写
+    Parameters
+    ----------
+    a : np.array
+    value : float
+    Returns
+    -------
+    tuple
+    '''
+    u,v=a.shape
+    for i in range(u):
+        for j in range(v):
+            if a[i,j]==value:
+                return (i,j)
 
 def generate_conv_kernel(cn,cross=True):
     '''
@@ -53,9 +76,8 @@ class Game:
         self.computer_history=[np.zeros((N,N),dtype=int)]
         self.player_history=[np.zeros((N,N),dtype=int)]
         self.algorithm=0
+        self.MCTS=None
        
-        
-
         
     def load(self,width=900,height=900):
         self.width=width
@@ -103,10 +125,12 @@ class Game:
         self.computer=np.zeros((N,N),dtype=int)
         self.computer_history=[np.zeros((N,N),dtype=int)]
         self.player_history=[np.zeros((N,N),dtype=int)]
+        self.MCTS=None
         
         self.cv.delete("all")
         self.canvas_grid()
-        self.refresh() 
+        self.refresh()
+        print("reseted")
         
     def set_algorithm(self):
         
@@ -227,10 +251,95 @@ class Game:
                 temp_x,temp_y=random.choice(choosable)#如果没希望了就随机走          
             self.computer[temp_x][temp_y]=1
         elif self.algorithm==1:
-            pass
+            if self.MCTS==None:      
+                try:
+                    with open("MCTS{}.sayoriaaa".format(self.N),"rb") as f:
+                        self.MCTS=pickle.load(f)
+                except IOError:
+                    self.train_MCTS(train_time=1000)
+                    
+            self.MCTS.simulation(self.player,self.computer)
+            _,next_move=self.MCTS.current_node.get_pi
+            self.computer[next_move]=1
+                
+    def train_MCTS(self,train_time=1000,batch_size=1):  
+        train_network=SNN(2,self.N)
+        training_MCTS=MCTS(board_size=self.N,nn=train_network)
+        N=self.N
+        Z=[]
         
+        for go in range(train_time):
+            pi=[]
+            player=np.zeros((N,N),dtype=int)  
+            computer=np.zeros((N,N),dtype=int)
+            computer_history=[np.zeros((N,N),dtype=int)]            
+            player_history=[np.zeros((N,N),dtype=int)]
+            training_MCTS.simulation(player, computer)
+            
+            while(True):
+                z0,z1,z2=check_even(player, computer),check_win_single(player),check_win_single(computer)
+                if not (z0 and z1 and z2):
+                    if z0:
+                        z=0
+                    elif z1:
+                        z=1
+                    else:
+                        z=-1
+                    Z.append(z)
+                    break
+                ret,next_move=training_MCTS.current_node.get_pi
+                pi.append(ret)
+                if training_MCTS.current_player==1:
+                    player[next_move]=1
+                    player_history.append(player)
+                else:
+                    computer[next_move]=1
+                    computer_history.append(computer)
+            
+##################################################
+#一局训练好打包
+                state=[]
+                Z_matrix=[]
+                z_matrix=fill_matrix(size=N,num=z)
+                z_matrix2=fill_matrix(size=N,num=-z)
+                for i in len(player_history):
+                    if i%2==0:
+                        data=np.stack([player_history[i],computer_history[i]])
+                        Z_matrix.append(z_matrix)
+                    else:
+                        data=np.stack([computer_history[i],player_history[i]])
+                        Z_matrix.append(z_matrix2)
+                    state.append(data)
+                    
+                tensor_states = torch.stack(tuple([torch.from_numpy(s) for s in state]))
+                tensor_pi = torch.stack(tuple([torch.from_numpy(p) for p in pi]))
+                tensor_z = torch.stack(tuple([torch.from_numpy(z) for z in Z_matrix]))
+                dataset = torch_data.TensorDataset(tensor_states, tensor_pi, tensor_z)
+                my_loader = torch_data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+                       
+                self.neural_network.train(my_loader) 
+            self.MCTS=training_MCTS
+                
+                        
+                        
+                        
+                    
+                    
+            
+            
+            
+            
+        
+        
+                   
     def destroy(self):
         self.win.destroy()
+def fill_matrix(size=0,num=1):
+    ret=np.zeros((size,size))
+    for i in range(size):
+        for j in range(size):
+            ret[i,j]=num
+    return ret
 
 def check_win_single(player):
     n,_=player.shape
@@ -321,7 +430,12 @@ class Node:
         if self.parent!=None:
             self.parent.backup(-v)
         
-    def compute(self):
+    def get_ucb(self):
+        '''
+        使用时要保证已经添加了子节点
+        否则需要先调用self.expand
+        simulation时使用ucb,实际走子时使用mcts分布
+        '''
         max_UCB=-np.inf
         ret_move=(0,0)
         for move,node in self.child.items():
@@ -331,24 +445,212 @@ class Node:
             if node.UCB>max_UCB:
                 max_UCB=node.UCB
                 ret_move=move
-        self.ret_move=ret_move
+        return ret_move
             
-    def add_child(self,move,P):
-        self.child[move]=Node(self,P)
+    def add_child(self,move):#先验概率在UCB中会赋值
+        self.child[move]=Node(self,player_label=-self.player_label)
     
     def lack_child(self):
         return not bool(self.child)
+    
+    def get_pi(self,train=True):
+        '''
+        mcts分布
+        根据当前父节点之下所有子节点的访问次数计算MCTS上的访问频率分布
+        Returns
+        -------
+        棋盘size的分布矩阵，以及运行或训练下的选择(tuple)
+        '''
+        ret=np.zeros(self.P.shape)
+        re_move=None
+        for pos,node in self.child:
+            ret[pos]=node.N
+        ret=ret/np.sum(ret)
+        if train:
+            ret_value=np.random.choice(ret.flatten(),p=0.8*ret.flatten()+0.2*np.random.dirichlet(np.ones(ret.shape[0]**2)))
+            re_move=get_matrix_index(ret, ret_value)
+        else:
+            re_move=divmod(np.argmax(ret), ret.shape[1]) 
+            
+        return ret ,re_move
+    
+        
+            
+            
+            
 
 class MCTS:
     def __init__(self,board_size=5,simulation_time=400,nn=None):
         self.board_size=board_size
         self.simulation_time=simulation_time
-        self.neural_network=nn
-        
+        self.neural_network=nn      
         self.root=Node(player_label=1)
+        self.current_node=self.root
+        self.current_player=1
+        
+
+        
+    def simulation(self,player,computer):
+        eval_counter=0
+        for step in range(self.simulation_time):
+            player_for_sim=np.array(player,copy=True)
+            computer_for_sim=np.array(computer,copy=True)
+            while self.check_prceed(player_for_sim, computer_for_sim):
+                if self.current_node.lack_child():
+                    self.P,v=self.neural_network.eva(self.get_state_for_eval(player,computer))
+                    eval_counter+=1
+                    self.current_node.backup(v)
+                    self.expand_treenode(player, computer)
+                next_move=self.current_node.get_ucb()
+                self.sim_move(player_for_sim, computer_for_sim, next_move)
+                self.current_node=self.current_node.child[next_move]
+                self.current_node.N+=1
+        return eval_counter/self.simulation_time#返回平均每次搜索的深度
+    
+    def get_state_for_eval(self,player,computer):
+        if self.current_player==1:
+            s=np.stack([player,computer])
+        else:
+            s=np.stack([computer,player])    
+        tensor_states = torch.from_numpy(s)
+        return tensor_states
+                
+            
+                
+                
+    def check_prceed(self,player,computer):
+        if check_even(player, computer):
+            return False
+        if self.current_player==1:
+            if check_win_single(computer):
+                self.current_node=self.current_node.parent
+                self.current_node.child={}
+                self.current_node.backup(-1)
+                return False
+        if self.current_player==-1:
+            if check_win_single(player):
+                self.current_node=self.current_node.parent
+                self.current_node.child={}
+                self.current_node.backup(1)
+                return False
+        return True
+    
+    def expand_treenode(self,player,computer):
+        size=player.shape[0]
+        for i in range(size):
+            for j in range(size):
+                if player[i,j]==0 and computer[i,j]==0:
+                    self.current_node.add_child((i,j),parent=self.current_node,player_label=-self.current_player)
+    def sim_move(self,player,computer,move):
+        if self.current_player==1:
+            player[move]=1
+        else:
+            computer[move]=1
+                    
+                    
+        
+                
+                
+
+class NN1(nn.Module):   
+    def __init__(self,input_layer,board_size=5):
+        super(NN1,self).__init__()
+        self.conv1=nn.Conv2d(input_layer, 32, 3)
+        self.conv2=nn.Conv2d(32, 64, 3)
+        self.conv3=nn.Conv2d(64, 128, 3)
+        
+        self.bn1=nn.BatchNorm2d(32)
+        self.bn2=nn.BatchNorm2d(64)
+        self.bn3=nn.BatchNorm2d(128)
+        self.relu=nn.ReLU()
+        self.tanh=nn.Tanh()
+        
+        ###############################
+        
+        self.v_conv1=nn.Conv2d(128,16, 1)
+        self.v_bn1=nn.BatchNorm2d(16)
+        self.v_fc1=nn.Linear(256, 256)
+        self.v_fc2=nn.Linear(256, 1)
+        
+        self.p_conv1=nn.Conv2d(128,16, 1)
+        self.p_bn1=nn.BatchNorm2d(16)
+        self.v_fc1=nn.Linear(256, board_size**2)
         
         
+    def forward(self,x):
+        x=self.relu(self.bn1(self.conv1(x)))
+        x=self.relu(self.bn2(self.conv2(x)))
+        x=self.relu(self.bn3(self.conv3(x)))
         
+        v=self.relu(self.v_bn1(self.v_conv1(x))).view(-1,256)
+        v=self.relu(self.v_fc1(v))
+        v=self.tanh(self.v_fc2(v))
+        
+        p=self.relu(self.p_bn1(self.p_conv1(x))).view(-1,256)
+        p=self.relu(self.p_fc1(p))
+        
+        return p,v
+    
+class SNN:
+    def __init__(self,input_layer,board_size,lr=0.1):
+        self.model=NN1(input_layer,board_size)
+        self.cuda=torch.cuda.is_available()
+        if self.cuda:
+            self.model=self.model.cuda().double()
+        self.opt=torch.optim.SGD(self.model.parameters(), momentum=0.9, lr=lr, weight_decay=1e-4)
+        self.mse=nn.MSELoss()
+        self.cross_entropy=nn.CrossEntropyLoss()
+        
+    def train(self,dataloader):
+        loss_record=[]
+        self.model.train()
+        for idx,(S,pi,Z) in dataloader:
+            
+            pi=Variable(pi).double()
+            Z=Variable(Z).double()
+            if self.cuda:
+                S=S.cuda()
+                pi=pi.cuda()
+                Z=Z.cuda()
+            
+            self.opt.zero_grad()
+            p,v=self.model(S)
+            output=F.log_softmax(p, dim=1)
+            cross_entropy = -torch.mean(torch.sum(pi*output, 1))
+            mse=F.mse_loss(v, Z)
+            loss=cross_entropy+mse
+            loss.backward()           
+            self.opt.step()
+            
+            if idx%10==0:
+                print("已训练{}局，mse{}，cross entropy:{}".format(idx,mse.data,cross_entropy.data))
+                loss_record.append((mse.data,cross_entropy.data))
+        return loss_record
+    
+    def eva(self,S):
+        S=Variable(S).double()
+        if self.cuda:
+            S=S.cuda()
+        with torch.no_grad():
+            p,v=self.model(S)
+        p=F.log_softmax(p, dim=1)
+        return p,v
+    
+                
+        
+    
+            
+            
+        
+    
+    
+        
+        
+    
+        
+        
+
+            
         
         
         
